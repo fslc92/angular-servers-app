34be2227519f16b5896d6872ec1d43dff4dfa94d
Sebastian Lozada <fslc92@gmail.com>
2018-09-06 17:34:31 -0500

    added new lecture changes, ngIf, ngModel, ngStyle

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,35 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]',
   //selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = 'TestServer';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
